c83946c1efc4d04a36d81132419458193581e1c3
Syed Atyab Hussain <syed.atyab.hussain.om@gmail.com>
2025-05-06 12:19:21 +0400

    Added a docker file and changed port

--- micro_apis/attendance_tracker/main.py
+++ micro_apis/attendance_tracker/main.py
@@ -222,4 +222,4 @@ def read_root():
     return FileResponse("static/index.html")
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=80, log_level="info")
+    uvicorn.run(app, host="0.0.0.0", port=8888, log_level="info")
